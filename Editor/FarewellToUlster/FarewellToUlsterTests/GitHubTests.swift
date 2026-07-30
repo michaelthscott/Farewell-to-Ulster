3d080d872adfab4a9491024ca1256b26a0be5612
Michael T H Scott <4441776+michaelthscott@users.noreply.github.com>
2026-07-30 17:52:17 +0100

    Update GitHubTests.swift

--- Editor/FarewellToUlster/FarewellToUlsterTests/GitHubTests.swift
+++ Editor/FarewellToUlster/FarewellToUlsterTests/GitHubTests.swift
@@ -13,15 +13,6 @@ import Testing
 
 struct GitHubTests {
 
-//    @Test func testGitHubBatchCommitter() async throws {
-//        let committer = GitHubBatchCommitter(owner: "michaelthscott", repo: "Farewell-to-Ulster", branch: "main")
-//        try await committer.commitFiles([
-//            "_Eras/01.md": "---\ntitle: Before Anything\n---\nThings I heard about or imagined from the earlier world.",
-//            "_Eras/02.md": "---\ntitle: Longshot\n---\nOur house and garden. A child at home.",
-//            "_Eras/03.md": "---\ntitle: Inchmarlo Boys\n---\nThe shock of going to school. All those other boys with their different family lives."
-//        ], message: "Testing batch commit")
-//    }
-//
 //    @Test func testGitHubClient() async throws {
 //        let client = GitHubClient(owner: "michaelthscott", repo: "Farewell-to-Ulster", branch: "main")
 //        let files = [LocalFile(path: "_Eras/01.md", content: Data("---\ntitle: Before Anything\n---\nThings I heard about or imagined from the earlier world.".utf8)),
